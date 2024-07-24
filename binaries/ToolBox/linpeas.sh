a7dff7f0f05e70924463ce6f75bca569ae779d85
kali <kali@kali.kali>
2024-07-24 00:56:10 -0700

    Pwn3d!

---
+++ binaries/ToolBox/linpeas.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f94002b26b9570c8cf791cb5adf2b721b678c1b1e07a40ca4aa39c5633b3d15
+size 860549
---
+++ binaries/jdwp-shellifier.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:724d012152ad7a3edf6777996cb71f8766905a86fc4240c6d98bc0601acaff5a
+size 310211
