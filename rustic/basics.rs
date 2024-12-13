4582b8f1dcb469fed1165e1818a3c7bb1c237c9e
tristanp299 <tristanp7313@gmail.com>
2024-12-13 14:55:14 -0800

    rust adv

--- rustic/basics.rs
+++ rustic/basics.rs
@@ -5,7 +5,507 @@ fn main() {
 	println!("Numbers {}",i);
 	}
 	}
-	
+
+// let1.rs
+fn main() {
+    let answer = 42;
+    println!("Hello {}", answer);
+}
+// let2.rs
+fn main() {
+    let answer = 42;
+    assert_eq!(answer,42);
+}
+// for1.rs
+fn main() {
+    for i in 0..5 {
+        println!("Hello {}", i);
+    }
+}
+// for2.rs
+fn main() {
+    for i in 0..5 {
+        if i % 2 == 0 {
+            println!("even {}", i);
+        } else {
+            println!("odd {}", i);
+        }
+    }
+}
+// for3.rs
+fn main() {
+    for i in 0..5 {
+        let even_odd = if i % 2 == 0 {"even"} else {"odd"};
+        println!("{} {}", even_odd, i);
+    }
+}
+// add1.rs
+fn main() {
+    let sum = 0;
+    for i in 0..5 {
+        sum += i;
+    }
+    println!("sum is {}", sum);
+}
+// add2.rs
+fn main() {
+    let mut sum = 0;
+    for i in 0..5 {
+        sum += i;
+    }
+    println!("sum is {}", sum);
+}
+// add3.rs
+fn main() {
+    let mut sum = 0.0;
+    for i in 0..5 {
+        sum += i as f64;
+    }
+    println!("sum is {}", sum);
+}
+// fun1.rs
+
+fn sqr(x: f64) -> f64 {
+    return x * x;
+}
+
+fn main() {
+    let res = sqr(2.0);
+    println!("square is {}", res);
+}
+// absolute value of a floating-point number
+fn abs(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        -x
+    }
+}
+
+// ensure the number always falls in the given range
+fn clamp(x: f64, x1: f64, x2: f64) -> f64 {
+    if x < x1 {
+        x1
+    } else if x > x2 {
+        x2
+    } else {
+        x
+    }
+}
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n-1)
+    }
+}
+fn by_ref(x: &i32) -> i32{
+    *x + 1
+}
+
+fn main() {
+    let i = 10;
+    let res1 = by_ref(&i);
+    let res2 = by_ref(&41);
+    println!("{} {}", res1,res2);
+}
+// fun4.rs
+
+fn modifies(x: &mut f64) {
+    *x = 1.0;
+}
+
+fn main() {
+    let mut res = 0.0;
+    modifies(&mut res);
+    println!("res is {}", res);
+}
+fn main() {
+    let x = 2.0 * std::f64::consts::PI;
+
+    let abs_difference = (x.cos() - 1.0).abs();
+
+    assert!(abs_difference < 1e-10);
+}
+use std::f64::consts;
+
+fn main() {
+    let x = 2.0 * consts::PI;
+
+    let abs_difference = (x.cos() - 1.0).abs();
+
+    assert!(abs_difference < 1e-10);
+}
+// array1.rs
+fn main() {
+    let arr = [10, 20, 30, 40];
+    let first = arr[0];
+    println!("first {}", first);
+
+    for i in 0..4 {
+        println!("[{}] = {}", i,arr[i]);
+    }
+    println!("length {}", arr.len());
+}
+// array2.rs
+// read as: slice of i32
+fn sum(values: &[i32]) -> i32 {
+    let mut res = 0;
+    for i in 0..values.len() {
+        res += values[i]
+    }
+    res
+}
+
+fn main() {
+    let arr = [10,20,30,40];
+    // look at that &
+    let res = sum(&arr);
+    println!("sum {}", res);
+}
+// array3.rs
+fn main() {
+    let ints = [1, 2, 3];
+    let floats = [1.1, 2.1, 3.1];
+    let strings = ["hello", "world"];
+    let ints_ints = [[1, 2], [10, 20]];
+    println!("ints {:?}", ints);
+    println!("floats {:?}", floats);
+    println!("strings {:?}", strings);
+    println!("ints_ints {:?}", ints_ints);
+}
+// slice1.rs
+fn main() {
+    let ints = [1, 2, 3, 4, 5];
+    let slice1 = &ints[0..2];
+    let slice2 = &ints[1..];  // open range!
+
+    println!("ints {:?}", ints);
+    println!("slice1 {:?}", slice1);
+    println!("slice2 {:?}", slice2);
+}
+// slice2.rs
+fn main() {
+    let ints = [1, 2, 3, 4, 5];
+    let slice = &ints;
+    let first = slice.get(0);
+    let last = slice.get(5);
+
+    println!("first {:?}", first);
+    println!("last {:?}", last);
+}
+// first Some(1)
+// last None
+println!("first {} {}", first.is_some(), first.is_none());
+println!("last {} {}", last.is_some(), last.is_none());
+println!("first value {}", first.unwrap());
+
+// first true false
+// last false true
+// first value 1
+let maybe_last = slice.get(5);
+let last = if maybe_last.is_some() {
+	*maybe_last.unwrap()
+} else {
+	-1
+};
+
+let last = *slice.get(5).unwrap_or(&-1);
+// vec1.rs
+fn main() {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+
+    let first = v[0];  // will panic if out-of-range
+    let maybe_first = v.get(0);
+
+    println!("v is {:?}", v);
+    println!("first is {}", first);
+    println!("maybe_first is {:?}", maybe_first);
+}
+// v is [10, 20, 30]
+// first is 10
+// maybe_first is Some(10)
+// vec2.rs
+fn dump(arr: &[i32]) {
+    println!("arr is {:?}", arr);
+}
+
+fn main() {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(20);
+    v.push(30);
+
+    dump(&v);
+
+    let slice = &v[1..];
+    println!("slice is {:?}", slice);
+}
+// iter1.rs
+fn main() {
+    let mut iter = 0..3;
+    assert_eq!(iter.next(), Some(0));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), None);
+}
+// iter2.rs
+fn main() {
+    let arr = [10, 20, 30];
+    for i in arr {
+        println!("{}", i);
+    }
+}
+// iter3.rs
+fn main() {
+    let arr = [10, 20, 30];
+    for i in arr.iter() {
+        println!("{}", i);
+    }
+
+    // slices will be converted implicitly to iterators...
+    let slice = &arr;
+    for i in slice {
+        println!("{}", i);
+    }
+// sum1.rs
+fn main() {
+    let sum: i32  = (0..5).sum();
+    println!("sum was {}", sum);
+
+    let sum: i64 = [10, 20, 30].iter().sum();
+    println!("sum was {}", sum);
+}
+// slice4.rs
+fn main() {
+    let ints = [1, 2, 3, 4, 5];
+    let slice = &ints;
+
+    for s in slice.windows(2) {
+        println!("window {:?}", s);
+    }
+}
+// window [1, 2]
+// window [2, 3]
+// window [3, 4]
+// window [4, 5]
+// OR
+for s in slice.chunks(2) {
+	println!("chunks {:?}", s);
+}
+// chunks [1, 2]
+// chunks [3, 4]
+// chunks [5]
+// vec3.rs
+fn main() {
+    let mut v1 = vec![10, 20, 30, 40];
+    v1.pop();
+
+    let mut v2 = Vec::new();
+    v2.push(10);
+    v2.push(20);
+    v2.push(30);
+
+    assert_eq!(v1, v2);
+
+    v2.extend(0..2);
+    assert_eq!(v2, &[10, 20, 30, 0, 1]);
+}
+// vec4.rs
+fn main() {
+    let mut v1 = vec![1, 10, 5, 1, 2, 11, 2, 40];
+    v1.sort();
+    v1.dedup();
+    assert_eq!(v1, &[1, 2, 5, 10, 11, 40]);
+}
+// string1.rs
+fn dump(s: &str) {
+    println!("str '{}'", s);
+}
+
+fn main() {
+    let text = "hello dolly";  // the string slice
+    let s = text.to_string();  // it's now an allocated string
+
+    dump(text);
+    dump(&s);
+}
+// string5.rs
+fn main() {
+    let mut s = String::new();
+    // initially empty!
+    s.push('H');
+    s.push_str("ello");
+    s.push(' ');
+    s += "World!"; // short for `push_str`
+    // remove the last char
+    s.pop();
+
+    assert_eq!(s, "Hello World");
+}
+// string6.rs
+fn array_to_str(arr: &[i32]) -> String {
+    let mut res = '['.to_string();
+    for v in arr {
+        res += &v.to_string();
+        res.push(',');
+    }
+    res.pop();
+    res.push(']');
+    res
+}
+
+fn main() {
+    let arr = array_to_str(&[10, 20, 30]);
+    let res = format!("hello {}", arr);
+
+    assert_eq!(res, "hello [10,20,30]");
+}
+// string2.rs
+fn main() {
+    let text = "static";
+    let string = "dynamic".to_string();
+
+    let text_s = &text[1..];
+    let string_s = &string[2..4];
+
+    println!("slices {:?} {:?}", text_s, string_s);
+}
+// slices "tatic" "na"
+// string3.rs
+fn main() {
+    let multilingual = "Hi! ¡Hola! привет!";
+    for ch in multilingual.chars() {
+        print!("'{}' ", ch);
+    }
+    println!("");
+    println!("len {}", multilingual.len());
+    println!("count {}", multilingual.chars().count());
+
+    let maybe = multilingual.find('п');
+    if maybe.is_some() {
+        let hi = &multilingual[maybe.unwrap()..];
+        println!("Russian hi {}", hi);
+    }
+}
+// 'H' 'i' '!' ' ' '¡' 'H' 'o' 'l' 'a' '!' ' ' 'п' 'р' 'и' 'в' 'е' 'т' '!'
+// len 25
+// count 18
+// Russian hi привет!
+let mut words = Vec::new();
+    words.extend(text.split_whitespace());
+let stripped: String = text.chars()
+.filter(|ch| ! ch.is_whitespace()).collect();
+// args0.rs
+fn main() {
+    for arg in std::env::args() {
+        println!("'{}'", arg);
+    }
+}
+// args1.rs
+use std::env;
+
+fn main() {
+    let first = env::args().nth(1).expect("please supply an argument");
+    let n: i32 = first.parse().expect("not an integer!");
+    // do your magic
+}
+match multilingual.find('п') {
+	Some(idx) => {
+		let hi = &multilingual[idx..];
+		println!("Russian hi {}", hi);
+	},
+	None => println!("couldn't find the greeting, Товарищ")
+};
+if let Some(idx) = multilingual.find('п') {
+	println!("Russian hi {}", &multilingual[idx..]);
+}
+let text = match n {
+	0 => "zero",
+	1 => "one",
+	2 => "two",
+	_ => "many",
+};
+let text = match n {
+	0...3 => "small",
+	4...6 => "medium",
+	_ => "large",
+ };
+ // file1.rs
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let first = env::args().nth(1).expect("please supply a filename");
+
+    let mut file = File::open(&first).expect("can't open the file");
+
+    let mut text = String::new();
+    file.read_to_string(&mut text).expect("can't read the file");
+
+    println!("file had {} bytes", text.len());
+
+}
+fn good_or_bad(good: bool) -> Result<i32,String> {
+    if good {
+        Ok(42)
+    } else {
+        Err("bad".to_string())
+    }
+}
+
+fn main() {
+    println!("{:?}",good_or_bad(true));
+    //Ok(42)
+    println!("{:?}",good_or_bad(false));
+    //Err("bad")
+
+    match good_or_bad(true) {
+        Ok(n) => println!("Cool, I got {}",n),
+        Err(e) => println!("Huh, I just got {}",e)
+    }
+    // Cool, I got 42
+
+}
+// file2.rs
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io;
+
+fn read_to_string(filename: &str) -> Result<String,io::Error> {
+    let mut file = match File::open(&filename) {
+        Ok(f) => f,
+        Err(e) => return Err(e),
+    };
+    let mut text = String::new();
+    match file.read_to_string(&mut text) {
+        Ok(_) => Ok(text),
+        Err(e) => Err(e),
+    }
+}
+
+fn main() {
+    let file = env::args().nth(1).expect("please supply a filename");
+
+    let text = read_to_string(&file).expect("bad file man!");
+
+    println!("file had {} bytes", text.len());
+}
+fn read_to_string(filename: &str) -> io::Result<String> {
+    let mut file = File::open(&filename)?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+    Ok(text)
+}
+
+// theredfoxandthelazydog
+
+// 11 42
 /*
 # Macros
 asert_eq!
---
+++ rustic/structs-enums-matching.rs
@@ -0,0 +1,68 @@
+// move1.rs
+fn main() {
+    let s1 = "hello dolly".to_string();
+    let s2 = s1;
+    println!("s1 {}", s1);
+}
+fn dump(s: &String) {
+    println!("{}", s);
+}
+
+fn main() {
+    let s1 = "hello dolly".to_string();
+    dump(&s1);
+    println!("s1 {}", s1);
+}
+dump(&"hello world".to_string());
+fn dump(s: &str) {
+    println!("{}", s);
+}
+{
+    let a = 10;
+    let b = "hello";
+    {
+        let c = "hello".to_string();
+        // a,b and c are visible
+    }
+    // the string c is dropped
+    // a,b are visible
+    for i in 0..a {
+        let b = &b[1..];
+        // original b is no longer visible - it is shadowed.
+    }
+    // the slice b is dropped
+    // i is _not_ visible!
+}
+// ref1.rs
+02 fn main() {
+    03    let s1 = "hello dolly".to_string();
+    04    let mut rs1 = &s1;
+    05    {
+    06        let tmp = "hello world".to_string();
+    07        rs1 = &tmp;
+    08    }
+    09    println!("ref {}", rs1);
+    10 }
+
+    / tuple1.rs
+
+fn add_mul(x: f64, y: f64) -> (f64,f64) {
+    (x + y, x * y)
+}
+
+fn main() {
+    let t = add_mul(2.0,10.0);
+
+    // can debug print
+    println!("t {:?}", t);
+
+    // can 'index' the values
+    println!("add {} mul {}", t.0,t.1);
+
+    // can _extract_ values
+    let (add,mul) = t;
+    println!("add {} mul {}", add,mul);
+}
+// t (12, 20)
+// add 12 mul 20
+// add 12 mul 20
